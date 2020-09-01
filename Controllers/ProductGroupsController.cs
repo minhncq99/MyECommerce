3c2497b4fab294b73b34d4a53afc39bd24f8a20b
Nguyen Cong Quang Minh <minhncq99@gmail.com>
2020-09-01 16:46:37 +0700

    Make Get Method For Product Groups

--- Controllers/ProductGroupsController.cs
+++ Controllers/ProductGroupsController.cs
@@ -1,43 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using MyECommerce.Models;
 
 namespace MyECommerce.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BusinessController : ControllerBase
+    public class ProductGroupsController : ControllerBase
     {
-
-        #region Resfull API
+        #region -- Restfull API --
         [HttpGet("get-all")]
-        public ActionResult<IEnumerable<Business>> GetAll()
+        public ActionResult<IEnumerable<ProductGroups>> GetAll()
         {
-            var result = _context.Business.ToList();
+            var result = _context.ProductGroups.ToList();
             return Ok(result);
         }
 
+        // Get By Id
         [HttpGet("get-by-id")]
-        public ActionResult<Business> GetById(int id)
+        public ActionResult<ProductGroups> GetById(int id)
         {
-            var result = _context.Business.FirstOrDefault(x => x.BusinessId == id);
+            var result = _context.ProductGroups.FirstOrDefault(x => x.ProductGroupId == id);
             return Ok(result);
         }
 
         // Create
 
         // Update
-        
+
         // Delete
+
         #endregion
+
         #region -- Initial --
         private readonly MyECommerceContext _context;
-        public BusinessController(MyECommerceContext context)
+
+        public ProductGroupsController(MyECommerceContext context)
         {
             _context = context;
         }
